1775926ab3408d0aa0a6780f1478c08a35aef503
Hari-Venkatesh-P <harivenkat1110@gmail.com>
2021-05-03 13:44:11 +0530

    Added RM API Url

--- utils/Constants.js
+++ utils/Constants.js
@@ -3,6 +3,7 @@ const constants = {
   BAD_REQUEST: "BAD_REQUEST",
   API_SUCCESS: "API_SUCCESS",
   API_FAILED: "API_FAILED",
+  RICKY_MORTY_API: "https://rickandmortyapi.com/api/character",
 };
 
 module.exports = constants;
